f703c78ce55d2c060a723f89f3920f613aaf0a03
Hackbright Student <info@hackbrightacademy.com>
2016-07-22 12:58:41 -0700

    Working tests.

--- party.py
+++ party.py
@@ -9,13 +9,61 @@ app.secret_key = "SECRETSECRETSECRET"
 
 
 def is_mel(name, email):
-    """Is this user Mel?"""
+    """Is this user Mel?
+
+        >>> is_mel("Mel Melitpolski", "mel@ubermelon.com")
+        True
+
+        >>> is_mel("Judith Butler", "mel@ubermelon.com")
+        True
+
+        >>> is_mel("Judith Butler", "judith@awesome.com")
+        False
+    """
 
     return name == "Mel Melitpolski" or email == "mel@ubermelon.com"
 
 
 def most_and_least_common_type(treats):
-    """Given list of treats, return {most, least} common types."""
+    """Given list of treats, return {most, least} common types.
+
+        >>> treats = [
+        ...     {'type': 'dessert'},
+        ...     {'type': 'dessert'},
+        ...     {'type': 'appetizer'},
+        ...     {'type': 'dessert'},
+        ...     {'type': 'appetizer'},
+        ...     {'type': 'drink'},
+        ... ]
+
+        >>> most_and_least_common_type(treats)
+        ('dessert', 'drink')
+
+
+        >>> treats = [
+        ...     {'type': 'dessert'},
+        ...     {'type': 'dessert'},
+        ... ]
+
+        >>> most_and_least_common_type(treats)
+        ('dessert', 'dessert')
+
+        >>> treats = [] 
+
+        >>> most_and_least_common_type(treats)
+        (None, None)
+
+        >>> treats = [
+        ...     {'type': 'dessert'},
+        ...     {'type': 'dessert'},
+        ...     {'type': 'appetizer'},
+        ...     {'type': 'appetizer'},
+        ...     {'type': 'drink'},
+        ... ]
+
+        >>> most_and_least_common_type(treats)
+        ('dessert', 'drink')
+    """
 
     types = {}
 
